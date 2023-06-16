f579e9e83e7b7f04847671ac3d1276d653766b14
Wesley Vance <wslyvnce@gmail.com>
2023-06-15 21:32:32 -0500

    added draft post views to test commenting action

---
+++ rareapi/views/post.py
@@ -0,0 +1,97 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import serializers, status
+from rareapi.models import Post, Comment, RareUser
+
+class PostView(ViewSet):
+    """Level up posts view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single post
+
+        Returns:
+            Response -- JSON serialized post
+        """
+        try:
+            post = Post.objects.get(pk=pk)
+            serializer = PostSerializer(post)
+            return Response(serializer.data)
+        except Post.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+
+    def list(self, request):
+        """Handle GET requests to get all posts
+
+        Returns:
+            Response -- JSON serialized list of posts
+        """
+        posts = Post.objects.all()
+        uid = request.META['HTTP_AUTHORIZATION']
+        user = RareUser.objects.get(uid=uid)
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+        
+        Returns
+            Response -- JSON serialized post instance
+        """
+        user_id = RareUser.objects.get(uid=request.data["userId"])
+
+        post = Post.objects.create(
+            title=request.data["title"],
+            content=request.data["content"],
+            # change below to datetime.now(), or in model?
+            publication_date=request.data["publicationDate"],
+            image_url=request.data["imageUrl"],
+            user_id=user_id
+        )
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for an post
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        post = Post.objects.get(pk=pk)
+        post.title = request.data["title"]
+        post.content = request.data["content"]
+        post.publication_date = request.data["publicationDate"]
+        post.image_url = request.data["imageUrl"]
+
+        post.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        """Handle DELETE requests for single post
+        
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post'], detail=True)
+    def add_comment(self, request, pk):
+        """Post request for a user to comment on a post"""
+
+        uid = request.META['HTTP_AUTHORIZATION']
+        user = RareUser.objects.get(uid=uid)
+        post = Post.objects.get(pk=pk)
+        comment = Comment.objects.create(
+            user_id=user,
+            post_id=post,
+            content=request.data["content"],
+            # created_on=DateTime.now()
+        )
+        return Response({'message': 'Comment Added'}, status=status.HTTP_201_CREATED)
