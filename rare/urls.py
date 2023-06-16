6e67fae7aa5d503745018fae210f4410834f359c
Mark Hamilton <ham.mark126@gmail.com>
2023-06-15 21:11:59 -0500

    added get requests for users, user serializer, auth view, updated urls

--- rare/urls.py
+++ rare/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rareapi.views.user_view import UserView
+from rareapi.views.auth import check_user, register_user
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'users', UserView, 'user')
+# router.register(r'rare_users', RareUserView, 'rare_user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('checkuser', check_user),
 ]
