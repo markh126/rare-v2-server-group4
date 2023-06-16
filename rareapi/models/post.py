25ed525543cdf92370218b4cb56bf3bee6340bd5
Tyler Eisen <jteisen90@gmail.com>
2023-06-16 08:06:44 +0700

    post, comments, rare_users, users, and subscriptions models adjusted and fixtures loaded

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -3,7 +3,7 @@ from .rare_user import RareUser
 
 class Post(models.Model):
     
-    user_id = models.ForeignKey(RareUser, on_delete=models.CASCADE, related_name='user')
+    user_id = models.ForeignKey(RareUser, on_delete=models.CASCADE, related_name='user',default=1)
     
     title = models.CharField(max_length=255)
     publication_date = models.DateField()
